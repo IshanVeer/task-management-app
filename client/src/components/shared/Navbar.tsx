91a0d94fd0d3b8f25a178b1a44ef80e2ef123008
Ishan Veer <ishanpveer@gmail.com>
2025-04-15 16:14:45 +0530

    added disabled type to button

--- client/src/components/shared/Navbar.tsx
+++ client/src/components/shared/Navbar.tsx
@@ -1,5 +1,6 @@
 import Button from "../ui/Button";
 import { useTheme } from "@/context/ThemeProvider";
+import { data } from "@/constants";
 
 interface Navbarprops {
   selectedBoard: string;
@@ -7,6 +8,9 @@ interface Navbarprops {
 
 const Navbar = ({ selectedBoard }: Navbarprops) => {
   const { mode } = useTheme();
+  const selectedBoardData = data.boards.find(
+    (board) => board.name === selectedBoard
+  );
   return (
     <div className="flex background-light900_dark400 justify-between items-center">
       {/* container left */}
@@ -21,7 +25,10 @@ const Navbar = ({ selectedBoard }: Navbarprops) => {
       <div className="border-l border-b px-8 py-6 flex-1  items-center flex  justify-between border-light">
         <h1 className="h1-bold">{selectedBoard}</h1>
         <div className="flex items-center gap-4">
-          <Button label="+ Add New Task" />
+          <Button
+            disabled={selectedBoardData?.columns?.length === 0}
+            label="+ Add New Task"
+          />
           <button className="cursor-pointer">
             <img src="/icons/icon-vertical-ellipsis.svg" alt="menu" />
           </button>
--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -3,12 +3,14 @@ import React from "react";
 interface ButtonProps {
   type?: string;
   label: string;
+  disabled?: boolean;
 }
 
-const Button = ({ type, label }: ButtonProps) => {
+const Button = ({ type, label, disabled }: ButtonProps) => {
   return (
     <button
-      className={` py-4 px-6 cursor-pointer transition duration-150 rounded-4xl base-bold ${
+      disabled={disabled}
+      className={` py-4 px-6 disabled:bg-primary-700 disabled:cursor-not-allowed cursor-pointer transition duration-150 rounded-4xl base-bold ${
         type === "secondary"
           ? "button-secondary "
           : type === "destruct"
