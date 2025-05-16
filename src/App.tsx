25afcea8a998181e737d6581d1e77bda4b9616c1
Ishan Veer <ishanpveer@gmail.com>
2025-05-16 13:27:22 +0530

    create project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Board from "./components/board/Board";
+import LeftSidebar from "./components/shared/LeftSidebar";
+import Navbar from "./components/shared/Navbar";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <>
+      <Navbar />
+      <div className="flex">
+        <LeftSidebar />
+        <Board />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/shared/LeftSidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftSidebar = () => {
+  return <div className="text-red-500">Left side bar</div>;
+};
+
+export default LeftSidebar;
