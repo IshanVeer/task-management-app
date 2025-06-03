d08228734fc89efdf89e925be81d36ffcad682e4
Ishan Veer <ishanpveer@gmail.com>
2025-06-03 13:05:22 +0530

    changed logo

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,11 +1,17 @@
+import { useTheme } from "@/context/ThemeProvider";
 import Button from "../ui/Button";
 
 const Navbar = () => {
+  const { mode } = useTheme();
   return (
     <div className="flex items-center px-8 h-24  w-full">
       {/* logo container */}
       <div className="w-[300px] ">
-        <img src="/icons/logo-dark.svg" alt="logo" />
+        {mode === "dark" ? (
+          <img src="/icons/logo-light.svg" alt="logo" />
+        ) : (
+          <img src="/icons/logo-dark.svg" alt="logo" />
+        )}
       </div>
       {/* Board heading container */}
       <div className="flex flex-1 border-l h-full items-center justify-between">
