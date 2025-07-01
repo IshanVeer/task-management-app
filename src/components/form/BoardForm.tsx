5e9a50cff4f59459db10816a3c0fb8649a0ed012
Ishan Veer <ishanpveer@gmail.com>
2025-07-01 16:09:21 +0530

    added error handling for board form

--- src/components/form/BoardForm.tsx
+++ src/components/form/BoardForm.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Button from "../ui/Button";
 import Card from "../ui/Card";
 import { useBoardData } from "@/context/BoardProvider";
+import Column from "../board/Column";
 
 interface BoardFormProps {
   mode?: string;
@@ -15,11 +16,17 @@ const BoardForm = ({ mode, closeModalHandler }: BoardFormProps) => {
 
   // Column state
   const [columns, setColumns] = useState<string[]>([""]);
+  const [boardNameInputError, setBoardNameInputError] = useState(false);
+  const [boardColumnInputErrors, setBoardColumnInputErrors] = useState<
+    boolean[]
+  >([]);
+  const [hasTriedSubmitting, setHasTriedSubmitting] = useState(false);
 
   useEffect(() => {
     if (mode === "edit" && selectedBoard) {
       setBoardName(selectedBoard?.name);
       setColumns(selectedBoard?.columns.map((column) => column.name));
+      setBoardColumnInputErrors(selectedBoard?.columns.map(() => false)); //initialise false error for each column
     }
   }, [mode, selectedBoard]);
 
@@ -27,6 +34,9 @@ const BoardForm = ({ mode, closeModalHandler }: BoardFormProps) => {
 
   const nameInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setBoardName(e.target.value);
+    if (boardNameInputError && e.target.value.trim() !== "") {
+      setBoardNameInputError(false);
+    }
   };
 
   // handle column change
@@ -34,6 +44,12 @@ const BoardForm = ({ mode, closeModalHandler }: BoardFormProps) => {
     const updatedColumns = [...columns];
     updatedColumns[index] = value;
     setColumns(updatedColumns);
+
+    if (boardColumnInputErrors[index] && value.trim() !== "") {
+      const updatedErrors = [...boardColumnInputErrors];
+      updatedErrors[index] = false;
+      setBoardColumnInputErrors(updatedErrors);
+    }
   };
 
   // add column
@@ -50,9 +66,18 @@ const BoardForm = ({ mode, closeModalHandler }: BoardFormProps) => {
 
   const submitBoardFormHandler = (e: React.FormEvent) => {
     e.preventDefault();
+    setHasTriedSubmitting(true);
     if (!boardName.trim()) {
+      setBoardNameInputError(true);
       return;
     }
+    const columnErrors = columns.map((column) => column.trim() === "");
+
+    if (columnErrors.some((error) => error)) {
+      setBoardColumnInputErrors(columnErrors);
+      return;
+    }
+
     if (mode === "edit" && selectedBoard) {
       editBoard(selectedBoard.id, boardName, columns);
     } else {
@@ -78,8 +103,16 @@ const BoardForm = ({ mode, closeModalHandler }: BoardFormProps) => {
             Name
           </label>
           <input
-            className="px-4 py-2 placeholder:text-[13px] border rounded-[4px]"
-            placeholder="e.g. Web Design"
+            className={`px-4 py-2 placeholder:text-[13px] focus:outline-none border rounded-[4px] ${
+              boardNameInputError && hasTriedSubmitting
+                ? "border-red-500 placeholder:text-red-500"
+                : ""
+            }`}
+            placeholder={`${
+              boardNameInputError && hasTriedSubmitting
+                ? "Can't be empty"
+                : "e.g. Web Design"
+            } `}
             id="name"
             type="text"
             value={boardName ?? ""}
@@ -95,8 +128,16 @@ const BoardForm = ({ mode, closeModalHandler }: BoardFormProps) => {
               className="w-full flex items-center gap-4 my-4"
             >
               <input
-                placeholder="e.g. Make coffee"
-                className="px-4 placeholder:text-[13px] w-full py-2 border rounded-[4px]"
+                placeholder={`${
+                  boardColumnInputErrors[index] && hasTriedSubmitting
+                    ? "Can't be empty"
+                    : "eg. To Do"
+                }`}
+                className={`${
+                  boardColumnInputErrors[index] && hasTriedSubmitting
+                    ? "placeholder:text-red-500 border-red-500"
+                    : ""
+                } px-4 placeholder:text-[13px] focus:outline-none w-full py-2 border rounded-[4px]`}
                 id="subtask"
                 type="text"
                 value={col}
